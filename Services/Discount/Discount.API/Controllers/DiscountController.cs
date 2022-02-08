a5a1f7fde39f49c867cf03a23b769597d1f72472
phuocleoceo <ht10082001@gmail.com>
2022-02-08 21:47:38 +0700

    Discount Repository

---
+++ Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,12 @@
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Discount.API.Entities;
+
+namespace Discount.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DiscountController : ControllerBase
+{
+
+}
