a3ee87767d6a8792fe6a4e715e3c16c31f51633b
phuocleoceo <ht10082001@gmail.com>
2022-02-26 22:45:39 +0700

    Validate Behaviour

--- Services/Ordering/Ordering.Application/Exceptions/ValidationException.cs
+++ Services/Ordering/Ordering.Application/Exceptions/ValidationException.cs
@@ -11,6 +11,8 @@ public class ValidationException : ApplicationException
 
     public ValidationException(IEnumerable<ValidationFailure> failures) : this()
     {
+        // Từ list các ValidationFailure, Group theo tên và error mess
+        // Chuyển thành Dict có Key là failureKey và Value là mảng các failure
         Errors = failures.GroupBy(c => c.PropertyName, c => c.ErrorMessage)
                         .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
     }
--- Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
+++ Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -5,9 +5,4 @@ namespace Ordering.Application.Features.Orders.Queries.GetOrdersList;
 public class GetOrdersListQuery : IRequest<List<OrdersVM>>
 {
     public string UserName { get; set; }
-
-    public GetOrdersListQuery(string userName)
-    {
-        UserName = userName;
-    }
 }
